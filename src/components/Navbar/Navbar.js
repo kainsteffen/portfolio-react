16db0b0d5b6dd2d81e5f8fa125b03d27b0c33c7f
kainsteffen <tranduykhanh1996@yahoo.de>
2019-06-13 18:08:26 +0200

    Remove redundant print

--- src/components/Navbar/Navbar.js
+++ src/components/Navbar/Navbar.js
@@ -50,7 +50,6 @@ class Navbar extends Component {
 
     handleScroll(event) {
         let scrollTop = window.pageYOffset;
-        console.log(scrollTop);
         if (scrollTop > 0) {
             this.setState(
                 {
