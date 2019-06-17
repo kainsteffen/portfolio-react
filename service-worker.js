c6b08bd22fca42e0aba48559f7892804f57a933d
kainsteffen <tranduykhanh1996@yahoo.de>
2019-06-17 02:44:14 +0200

    Updates

--- service-worker.js
+++ service-worker.js
@@ -14,7 +14,7 @@
 importScripts("https://storage.googleapis.com/workbox-cdn/releases/3.6.3/workbox-sw.js");
 
 importScripts(
-  "//kainsteffen.github.io/portfolio-react/precache-manifest.3a74d487cedc3c5cb0c10508abe1a27e.js"
+  "/precache-manifest.d2d6d1a7ce6b905669155bc705fb145f.js"
 );
 
 workbox.clientsClaim();
@@ -28,7 +28,7 @@ self.__precacheManifest = [].concat(self.__precacheManifest || []);
 workbox.precaching.suppressWarnings();
 workbox.precaching.precacheAndRoute(self.__precacheManifest, {});
 
-workbox.routing.registerNavigationRoute("//kainsteffen.github.io/portfolio-react/index.html", {
+workbox.routing.registerNavigationRoute("/index.html", {
   
   blacklist: [/^\/_/,/\/[^\/]+\.[^\/]+$/],
 });
