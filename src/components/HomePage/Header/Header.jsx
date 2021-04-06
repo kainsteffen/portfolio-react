bfb808a07bffdc71d775c56b313357dae743e067
kainsteffen <tranduykhanh1996@yahoo.de>
2021-04-07 01:54:33 +0200

    feat(header): add wave animation

--- src/components/HomePage/Header/Header.jsx
+++ src/components/HomePage/Header/Header.jsx
@@ -39,7 +39,8 @@ class Header extends Component {
         </div>
         <div className="header-quote fadeIn-slideInFromBelow">
           <div className="header-titles">
-            <h1>Hi, I&apos;m Khanh 👋</h1>
+            <h1>Hi, I&apos;m Khanh</h1>
+            <h1 className="wave">👋</h1>
             <h2> I&apos;m good at pushing pixels around the screen.</h2>
           </div>
         </div>
