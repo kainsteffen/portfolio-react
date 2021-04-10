0d1646850f46e8b9247ef629903b2cfe1c7e6a66
kainsteffen <tranduykhanh1996@yahoo.de>
2021-04-10 19:03:41 +0200

    feat(media-queries): add media query mixin

--- src/index.jsx
+++ src/index.jsx
@@ -2,8 +2,8 @@ import React from "react";
 import ReactDOM from "react-dom";
 import { BrowserRouter as Router } from "react-router-dom";
 import * as serviceWorker from "./serviceWorker";
-import "./index.css";
-import "./animations.css";
+import "./index.scss";
+import "./animations.scss";
 import App from "./App";
 
 const app = (
