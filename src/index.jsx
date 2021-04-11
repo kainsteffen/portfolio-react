66de64de5725de159241597743e75171251e8b44
kainsteffen <tranduykhanh1996@yahoo.de>
2021-04-11 16:18:23 +0200

    fix(index): fix page 404 on route reload

--- src/index.jsx
+++ src/index.jsx
@@ -1,6 +1,6 @@
 import React from "react";
 import ReactDOM from "react-dom";
-import { BrowserRouter as Router } from "react-router-dom";
+import { HashRouter as Router } from "react-router-dom";
 import * as serviceWorker from "./serviceWorker";
 import "./index.scss";
 import "./animations.scss";
